ad3b0e15653c82e2352e44e5eddc8c71a70a8bf4
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-07-17 17:24:40 +0200

    added  search bar

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -14,6 +14,7 @@ import { TrailerComponent } from '../trailer/trailer.component';
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent {
+  titleFilter: string = '';
 movies: any[] = [];
 loading: boolean = true;
 constructor (
@@ -27,6 +28,19 @@ constructor (
 ngOnInit (): void {
   this.getMovies();
 
+}
+
+/**
+ * @method applyFilter
+ * @description filters the movies by title
+ * @memberof MovieCardComponent
+ */
+
+applyFilter(): void {
+  if(this.titleFilter.trim() == "" && this.titleFilter.length > 0) {
+  this.getMovies();
+   } else
+  this.movies = this.movies.filter((movie: any) => movie.Title.toLowerCase().includes(this.titleFilter.toLowerCase()));
 }
 
 /**
@@ -48,6 +62,7 @@ getMovies(): void {
     console.log(this.movies);
   });
 }
+
 
 
 /**
